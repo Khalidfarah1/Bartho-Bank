8bfe2f9fc7f13ace8c14e8fee94ed943efaca273
Khalidfarah1 <khalidfarah9@gmail.com>
2026-03-31 22:39:58 +0100

    feat: add PrivateRoute guard, /login and /register routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './components/Layout'
 import Dashboard from './pages/Dashboard'
 import Transfer from './pages/Transfer'
 import AccountDetails from './pages/AccountDetails'
 import BankDetails from './pages/BankDetails'
 import Spending from './pages/Spending'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import { isAuthenticated } from './lib/api'
+
+function PrivateRoute({ children }: { children: React.ReactNode }) {
+  return isAuthenticated() ? <>{children}</> : <Navigate to="/login" replace />
+}
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route
+        path="/"
+        element={
+          <PrivateRoute>
+            <Layout />
+          </PrivateRoute>
+        }
+      >
         <Route index element={<Dashboard />} />
         <Route path="transfer" element={<Transfer />} />
         <Route path="spending" element={<Spending />} />
